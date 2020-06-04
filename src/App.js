7e68ca9829bb80cd95d95808f70ef3125b13a36c
ansarulSohan <54618940+ansarulSohan@users.noreply.github.com>
2020-06-04 16:00:43 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>This is my first React app</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <PersonInfo></PersonInfo>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Contributors name="G. M. Ansarul Kabir" degree="B.Sc in CSE" email="ansarulsohan@gmail.com"></Contributors>
       </header>
     </div>
   );
 }
 
+function PersonInfo() {
+  return (
+    <div>
+      <h2 style={{fontFamily:'consolas', color: 'red'}}>Hallo!</h2>
+      <p>Welcome to React! This is a cool framework</p>
+    </div>
+  );
+}
+
+function Contributors(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>{props.degree}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
 export default App;
